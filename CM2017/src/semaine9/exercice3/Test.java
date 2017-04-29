5030a3cfb023c0ac7b6c783ea1dc66b1a771006b
mnv <mnv@L139.ephec.be>
2017-04-29 11:39:10 +0200

    Un damier de JButton

---
+++ CM2017/src/semaine9/exercice3/Test.java
@@ -0,0 +1,73 @@
+package semaine9.exercice3;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+
+public class Test extends JFrame implements ActionListener {
+
+	private int nbLignes = 5;
+	private int nbColonnes = 6;
+	private JPanel contentPane;
+	private JButton[][] tabJButton = new JButton[nbColonnes][nbLignes];
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Test frame = new Test();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Test() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0};
+//		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+//		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		for (int x = 0 ; x < nbColonnes ; x++){
+			for (int y = 0 ; y < nbLignes ; y++){
+				tabJButton[x][y] = new JButton(x+","+y);
+				tabJButton[x][y].addActionListener(this);
+				GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+				gbc_btnNewButton.gridx = x;
+				gbc_btnNewButton.gridy = y;
+				contentPane.add(tabJButton[x][y], gbc_btnNewButton);
+			}
+		}
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		System.out.println("on a cliqué en "+evt.getActionCommand());
+		
+	}
+
+}
